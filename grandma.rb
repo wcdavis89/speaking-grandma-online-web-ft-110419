a7e41e3eca0607be2f77cdae1140633cd9ec3261
Whittney Davis <wcdavis89@yahoo.com>
2019-11-07 14:51:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 def speak_to_grandma(string)
   if string==string.upcase
     p "NO, NOT SINCE 1938!"
-    elsif
-    string=="I LOVE YOU GRANDMA!"
-    p "I LOVE YOU TOO PUMPKIN!"
   else
     p "HUH?! SPEAK UP, SONNY!"
   end
